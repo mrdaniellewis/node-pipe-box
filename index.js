d8ba3d84bb2b915b0aa87ad3eed7d15b154e91d8
Daniel Lewis <github@mrdaniellewis.co.uk>
2017-12-01 21:52:38 +0000

    update

--- index.js
+++ index.js
@@ -1,85 +1,48 @@
-/* jshint node: true */
-"use strict";
+'use strict';
 
-var stream = require('stream');
-var util = require('util');
+const { Transform } = require('stream');
 
-/**
- *	This class is intended to be extended
- *	The constructor should to set a _inputStream and _outputStream object,
- * 	the writable side of the class will then write to _inputStream and the 
- *	readable side read from _outputStream.
- *
- *	Alternativly the output and input streams can be passed as options
- *	to this constructor
- *
- *	If the output and input stream is not specified it becomes an overcomplex
- *	passthough stream
- */
-function PipeBox(options) {	
-	
-	options = options || {};
+class PipeBox extends Transform {
+  constructor(options = {}) {
+    const { writeable, readable, ...rest } = options;
+    super(rest);
+    if (writeable) {
+      this.writeable = writeable;
+    }
+    if (readable) {
+      this.readable = readable;
+    }
 
-	var duplexOptions = {};
-	for ( var i in options ) {
-		duplexOptions[i] = options[i];
-	}
-	duplexOptions.allowHalfOpen = true;
+    this._initiated = false;
+  }
 
-	stream.Duplex.call( this, duplexOptions );
+  _init() {
+    this._initiated = true;
+    this.readable.on('readable', () => {
+      let chunk;
+      while ((chunk = this.readable.read()) !== null) {
+        if (this.push(chunk) === false) {
+          break;
+        }
+      }
+    });
+  }
 
-	this._inputStream = this._inputStream || options.inputStream || new stream.PassThrough();
-	this._outputStream = this._outputStream || options.outputStream || this._inputStream;
+  _transform(chunk, encoding, callback) {
+    if (!this._initiated) {
+      this._init();
+    }
+    if (!this.writeable.write(chunk, encoding)) {
+      this.writeable.once('drain', () => this.uncork());
+      this.cork();
+    }
+    callback();
+  }
 
-	this._outputStream
-		.on( 'end', function() {
-			// Send the end chunk
-			this.push(null);
-		}.bind(this) )
-		.on( 'finish', function() {
-			// For some reason the input stream can require
-			// a little kick to output end event in some circumstances
-			// I'd write a test case if I could figure it out
-			this.read(0);
-		} );
-
-	this.on( 'finish', function() {
-		// We've got all the data in, end the inputstream
-		this._inputStream.end();
-	}.bind(this) );
-
+  _flush(callback) {
+    this.readable.on('end', () => callback());
+    this.writeable.end();
+  }
 }
-
-util.inherits( PipeBox, stream.Duplex );
-
-PipeBox.prototype._write = function( chunk, encoding, cb ) {
-	
-	// Pass the data to the input
-	// If the input is buffering wait for a drain before allowing to continue
-	if ( !this._inputStream.write( chunk, encoding ) ) {
-		this._inputStream.once( 'drain', function() {
-			cb();
-		} );
-	} else {
-		cb();
-	}
-
-};
-
-PipeBox.prototype._read = function() {
-
-	// The size argument is not the size argument passed by the user
-	// It appears to actually be the highWaterMark
-	// This means it can't practially be used
-
-	// Just try and read until nothing is returned or pipebox tells us to stop
-	var chunk;
-	while( null !== ( chunk = this._outputStream.read() ) ) {
-		if ( !this.push( chunk ) ) {
-			break;
-		}
-	}
-
-};
 
 module.exports = PipeBox;
---
+++ test.js
@@ -0,0 +1,34 @@
+/* eslint-env jest */
+
+'use strict';
+
+const PipeBox = require('./');
+const { Transform, PassThrough } = require('stream');
+const { Collect } = require('stream-collect');
+const fs = require('fs');
+const pathUtils = require('path');
+
+const path = pathUtils.resolve(__dirname, 'the-machine-stops.txt');
+const data = fs.readFileSync(path, { encoding: 'utf8' });
+
+describe('PipeBox', () => {
+  it('is a duplex stream', () => {
+    const readable = new PassThrough();
+    const writeable = readable;
+    expect(new PipeBox({ readable, writeable })).toBeInstanceOf(Transform);
+  });
+
+  it('encapsulates a chain of streams', () => {
+    const readable = new PassThrough();
+    const writeable = readable;
+    const pipeBox = new PipeBox({ readable, writeable });
+
+    return fs.createReadStream(path, { encoding: 'utf8' })
+      .pipe(pipeBox)
+      .pipe(new Collect())
+      .collect()
+      .then((collected) => {
+        expect(collected.toString()).toEqual(data);
+      });
+  });
+});
