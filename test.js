23d7fe008f06cf0afd6cff99e31ff42930c2663b
Daniel Lewis <github@mrdaniellewis.co.uk>
2018-10-09 23:28:46 +0100

    dependency update

--- test.js
+++ test.js
@@ -2,11 +2,11 @@
 
 'use strict';
 
-const PipeBox = require('./');
 const { Transform, PassThrough } = require('stream');
 const { Collect } = require('stream-collect');
 const fs = require('fs');
 const pathUtils = require('path');
+const PipeBox = require('./');
 
 const path = pathUtils.resolve(__dirname, 'the-machine-stops.txt');
 const data = fs.readFileSync(path, { encoding: 'utf8' });
